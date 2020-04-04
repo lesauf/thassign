78b211c7b17dbcf556c1f6feeb39342ae837ce3c
Fotso Teketchuet Stéphane <lesauf@gmail.com>
2020-04-04 03:39:39 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+
+// import { AuthService } from './modules/auth/auth.service';
+
+// import { SharedModule } from './shared.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'thassign';
+export class AppComponent implements OnInit, OnDestroy {
+  /**
+   * @description Property that stores the selected language value
+   * from the component
+   */
+  public language: string;
+
+  private userSubscription: Subscription;
+  public user: any;
+
+  constructor(
+    // private authService: AuthService,
+    private router: Router,
+    private translate: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+    // init this.user on startup
+    // this.authService.me().subscribe(data => {
+    //   this.user = data.user;
+    // });
+
+    // update this.user after login/register/logout
+    // this.userSubscription = this.authService.$userSource.subscribe(user => {
+    //   this.user = user;
+    // });
+
+    this._initTranslationLanguage();
+  }
+
+  logout(): void {
+    // this.authService.signOut();
+    this.navigate('');
+  }
+
+  navigate(link: any): void {
+    this.router.navigate([link]);
+  }
+
+  ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
+
+  /**
+   * Set the default language for translation strings
+   * and the current language.
+   */
+  private _initTranslationLanguage() {
+    this.translate.setDefaultLang('en');
+
+    if (this.translate.getBrowserLang() !== undefined) {
+      this.translate.use(this.translate.getBrowserLang());
+    } else {
+      this.translate.use('fr'); // Set the current language
+    }
+
+    // Setting locale for dates
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  TranslateModule,
+  TranslateCompiler,
+  TranslateLoader,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateMessageFormatCompiler } from 'ngx-translate-messageformat-compiler';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      compiler: {
+        provide: TranslateCompiler,
+        useClass: TranslateMessageFormatCompiler,
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  // return new TranslateHttpLoader(http);
+}
